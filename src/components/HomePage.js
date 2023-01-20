bb31e8c56ad78a5f23ba771acf8d26399c042eff
Diana Wu <dianawu005@gmail.com>
2023-01-20 01:19:18 -0800

    add HomePage and Menu feature

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,58 @@
+import { Menu } from 'antd';
+import { useState } from 'react';
+
+const items = [
+    {
+        label: 'Announcements',
+        key: 'announcemets',
+    },
+    {
+        label: 'Maintenance',
+        key: 'maintenance',
+    },
+    {
+        label: 'Bills',
+        key: 'bills',
+        
+    },];
+
 const HomePage = () => {
 
+    const[current, setCurrent] = useState('announcements');
+
+    const onItemClick = (e) => {
+        setCurrent(e.key);
+    };
+
+    // change to the selected component
+    // later will be changed to <Maintenance />; <Bills />; <Announcement />
+    const switchPage = () => {
+        switch (current) {           
+            case "maintenance":
+                return "Maintenance";
+            case "bills": 
+                return "Bills";
+            default:
+                return "Announcement";
+        }  
+    };
     return (
-        <div>
-            HomePage
+        <div style={{ fontSize: 20, fontWeight: 600 }}>
+            <div style={{paddingLeft:"50%", 
+            borderBottomColor:"#282c34", borderBottomWidth:"4px", 
+            borderBottom:"6px ridge rgba(66, 70, 92, .3)"}}>
+            <Menu 
+                onClick={onItemClick} 
+                selectedKeys={[current]} 
+                mode="horizontal" 
+                items={items} 
+                style={{backgroundColor: "#f0f2f5", fontSize: 18}}
+
+            />
+            </div>
+            <div >
+                {switchPage()}
+            </div>
         </div>
     );
 }

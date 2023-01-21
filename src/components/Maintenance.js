d943146f2f2617ea06fce92306668809d4d77e79
Diana Wu <dianawu005@gmail.com>
2023-01-21 14:38:16 -0800

    Set Status changable.

--- src/components/Maintenance.js
+++ src/components/Maintenance.js
@@ -1,6 +1,104 @@
 import { Button, Space, Table, Tag } from 'antd';
-// to define each column
-const columns = [
+import { useEffect, useState } from 'react';
+
+  // This is a fake data list for testing only. Need to contact backend and make sure the status is string[] type
+
+
+
+const Maintenance = () => {
+
+  const rows=[
+    {
+      requestId:"1",
+      title:"roof is broken",
+      date:"12/01/2020",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['approved'],
+    },
+    {
+      requestId:"21",
+        title:"roof is broken",
+      date:"11/01/2020",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['open'],
+    },
+    {
+      requestId:"15",
+     title:"roof is broken",
+      date:"12/01/2021",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['open'],
+    },
+    {
+      requestId:"12",
+        title:"roof is broken",
+      date:"02/01/2020",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['open'],
+    },
+    {
+      requestId:"13",
+        title:"roof is broken",
+      date:"12/01/2022",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['closed'],
+    },
+    {
+      requestId:"6",
+        title:"roof is broken",
+      date:"12/15/2020",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['closed'],
+    },
+    {
+      requestId:"10",
+      title:"roof is broken",
+      date:"11/11/2020",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['open'],
+    },
+    {
+      requestId:"4",
+      title:"roof is broken",
+      date:"12/10/2020",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['open'],
+    },
+    {
+      requestId:"18",
+      title:"roof is broken",
+      date:"10/01/2020",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['open'],
+    },
+    {
+      requestId:"31",
+        title:"roof is broken",
+      date:"12/01/2020",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['open'],
+    },
+    {
+      requestId:"22",
+        title:"roof is broken",
+      date:"12/01/2020",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['closed'],
+    },
+    {
+      requestId:"26",
+        title:"roof is broken",
+      date:"12/01/2020",
+      description: "The roof is somehow broken. Please fix it.",
+      status:['closed'],
+    },
+  ];
+
+  const[data, setData]=useState(rows);
+  
+
+  // to define each column
+  const columns = [
     {
       title: 'Submit Date',
       dataIndex: 'date',
@@ -21,13 +119,16 @@ const columns = [
       width: 120,
       filters:[
         {
-            text: "open",
-            value:"open",
-
+          text: "open",
+          value:"open",
         },
         {
-            text:"closed",
+          text:"closed",
             value:"closed",
+        },
+        {
+          text:"approved",
+            value:"approved",
         },
       ],
       onFilter: (value, record) => record.status.indexOf(value) === 0,
@@ -35,9 +136,8 @@ const columns = [
       render: (_, { status }) => (
         <>
           {status.map((recordStatus) => {
-            let color = recordStatus === 'open' ? 'green' : 'lightgrey';
-          
-            
+            let color = recordStatus === 'closed' ? 'lightgrey' : (recordStatus === 'approved'?'blue':'green');
+
             return (
               <Tag color={color} key={recordStatus}>
                 {recordStatus.toUpperCase()}
@@ -52,102 +152,23 @@ const columns = [
       key: 'action',
       width: 180,
 
-      render: () => (
-        <Space size="middle">
-          <a>View</a>
-          <a>
-            <Space>
-              Edit
-            </Space>
-          </a>
-          <a>
-            <Space>
-              Delete
-            </Space>
-          </a>
-        </Space>
-      ),
+      render: (_,record) =>
+
+        data.length >= 1 ? (
+          <Space size="middle">
+            <Button> View </Button>
+            <Button> Edit </Button>
+            <a onClick={() => handleDelete(record.requestId)}>Delete</a>
+          </Space>
+        ) : null
     },
   ];
 
-  // This is a fake data list for testing only. Need to contact backend and make sure the status is string[] type
-const data=[
-    {
-      title:"roof is broken",
-      date:"12/01/2020",
-      description: "The roof is somehow broken. Please fix it.",
-      status:['open'],
-    },
-    {
-        title:"roof is broken",
-      date:"11/01/2020",
-      description: "The roof is somehow broken. Please fix it.",
-      status:['open'],
-    },
-    {
-     title:"roof is broken",
-      date:"12/01/2021",
-      description: "The roof is somehow broken. Please fix it.",
-      status:['open'],
-    },
-    {
-        title:"roof is broken",
-      date:"02/01/2020",
-      description: "The roof is somehow broken. Please fix it.",
-      status:['open'],
-    },
-    {
-        title:"roof is broken",
-      date:"12/01/2022",
-      description: "The roof is somehow broken. Please fix it.",
-      status:['closed'],
-    },
-    {
-        title:"roof is broken",
-      date:"12/15/2020",
-      description: "The roof is somehow broken. Please fix it.",
-      status:['closed'],
-    },
-    {
-        title:"roof is broken",
-        date:"11/11/2020",
-        description: "The roof is somehow broken. Please fix it.",
-        status:['open'],
-      },
-      {
-          title:"roof is broken",
-        date:"12/10/2020",
-        description: "The roof is somehow broken. Please fix it.",
-        status:['open'],
-      },
-      {
-       title:"roof is broken",
-        date:"10/01/2020",
-        description: "The roof is somehow broken. Please fix it.",
-        status:['open'],
-      },
-      {
-          title:"roof is broken",
-        date:"12/01/2020",
-        description: "The roof is somehow broken. Please fix it.",
-        status:['open'],
-      },
-      {
-          title:"roof is broken",
-        date:"12/01/2020",
-        description: "The roof is somehow broken. Please fix it.",
-        status:['closed'],
-      },
-      {
-          title:"roof is broken",
-        date:"12/01/2020",
-        description: "The roof is somehow broken. Please fix it.",
-        status:['closed'],
-      },
-];
-
-
-const Maintenance = () => {
+// TODO
+  const handleDelete = (requestId) => {
+    const newData = data.filters((item) => item.requestId !== requestId);
+    setData(newData);
+  };
 
     return(
         <div>

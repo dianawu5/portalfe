860d631f15e0a963d1acabf9a3151e7e1551fecb
Diana Wu <dianawu005@gmail.com>
2023-01-22 17:16:11 -0800

    add delete function

--- src/components/Maintenance.js
+++ src/components/Maintenance.js
@@ -1,4 +1,4 @@
-import { Button, message, Space, Table} from 'antd';
+import { Button, message, Space, Table, Popconfirm,} from 'antd';
 import { useEffect, useState } from 'react';
 import { deleteRequest } from '../utils';
 import NewRequestButton from './NewRequestButton';
@@ -16,10 +16,115 @@ and
 }
 
 */
-// This is a fake data list for testing only.
-const Maintenance = () => {
 
-  const rows=[
+const Maintenance = () => {
+  const requestsList = maintenanceList();
+  const[data, setData]=useState(requestsList);
+
+  // to define each column
+  const columns = [
+    {
+      title: 'Submit Date',
+      dataIndex: 'date',
+      key: 'date',
+      width: 120,
+    },
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: 'Status',
+      key: 'status',
+      dataIndex: 'status',
+      width: 120,
+      filters:[
+        {
+          text: "open",
+          value:"open",
+        },
+        {
+          text:"closed",
+            value:"closed",
+        },
+        {
+          text:"approved",
+            value:"approved",
+        },
+      ],
+      onFilter: (value, record) => record.status.indexOf(value) === 0,
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      width: 180,
+
+      render: (_, record) =>
+        record.status === "Open" ? (
+          <Space size="large">
+            <a>View</a>
+            <a>Edit</a>
+            <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.requestId)}>
+              <a>Delete</a>
+            </Popconfirm>
+          </Space>
+      ) : (<a>View</a>),
+    },
+  ];
+
+  const handleDelete = (requestId) => {
+    const newData = data.filter((item) => item.requestId !== requestId);
+    setData(newData);
+  };
+
+/* once connected to the server use this coding
+
+  const[loading,setLoading] = useState(false);
+
+  const[data, setData] = useState();
+  useEffect(() => {
+    getRequest().then(requests) => {setData(requests);} catch(error){message.error(error.message);}
+  }, []);
+
+  const handleDelete = async(requestId) => {
+    setLoading(true); 
+    try{
+      await deleteRequest(requestId);
+      message.success("You delete a reqeust successfully!");
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+*/ 
+
+    return(
+        <div>
+            <div style={{ fontSize: 20, fontWeight: 600,paddingBottom:"20px" }}>
+            Maintenance
+            </div>
+            <div style={{ fontSize: 20, fontWeight: 600,paddingLeft:"15%" }}>
+
+                <div style={{padding: "10px"}} align="end">
+                  <NewRequestButton />
+                </div>
+                <div style={{ padding:"5px",backgroundColor:"rgba(55, 88, 70,.1)"}}>
+                    <div style={{ fontSize: 18, fontWeight: 600,padding:"5px",backgroundColor:"rgba(55, 88, 70,.1)"}}
+                    >
+                        Requests Status
+                    </div>
+                    <Table columns={columns} dataSource={data} size="small" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+// This is a fake data list for testing only.
+const maintenanceList = () => {
+  const reuestsList=[
     {
       requestId:"1",
       title:"roof is broken",
@@ -105,103 +210,7 @@ const Maintenance = () => {
       status:"Closed",
     },
   ];
-
-  const[data, setData]=useState(rows);
-  
-  
-  // to define each column
-  const columns = [
-    {
-      title: 'Submit Date',
-      dataIndex: 'date',
-      key: 'date',
-      width: 120,
-    },
-    {
-      title: 'Title',
-      dataIndex: 'title',
-      key: 'title',
-
-    },
-    {
-      title: 'Status',
-      key: 'status',
-      dataIndex: 'status',
-      width: 120,
-      filters:[
-        {
-          text: "open",
-          value:"open",
-        },
-        {
-          text:"closed",
-            value:"closed",
-        },
-        {
-          text:"approved",
-            value:"approved",
-        },
-      ],
-      onFilter: (value, record) => record.status.indexOf(value) === 0,
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      width: 180,
-
-      render: (_,record) =>
-
-        data.length >= 1 ? (
-          <Space size="middle">
-            <Button> View </Button>
-            <Button> Edit </Button>
-            <a>Delete</a>
-          </Space>
-        ) : null
-    },
-  ];
-
-/* once connected to the server use this coding
-
-  const[loading,setLoading] = useState(false);
-
-  const handleDelete = async(requestId) => {
-    setLoading(true);
-    const newData = data.filters((item) => item.requestId !== requestId);
-    try{
-      await deleteRequest(requestId);
-      setData(newData);
-      message.success("You delete a reqeust successfully!");
-    } catch (error) {
-      message.error(error.message);
-    }
-  };
-*/ 
-/*  const handleDelete = (requestId) => {
-    const newData = data.filters((item) => item.requestId !== requestId);
-    setData(newData);
-  };
-*/
-
-    return(
-        <div>
-            <div style={{ fontSize: 20, fontWeight: 600,paddingBottom:"20px" }}>
-            Maintenance
-            </div>
-            <div style={{ fontSize: 20, fontWeight: 600,paddingLeft:"15%" }}>
-
-                <div style={{padding: "10px"}} align="end">
-                  <NewRequestButton />
-                </div>
-                <div style={{ padding:"5px",backgroundColor:"rgba(55, 88, 70,.1)"}}>
-                    <div style={{ fontSize: 18, fontWeight: 600,padding:"5px",backgroundColor:"rgba(55, 88, 70,.1)"}}
-                    >
-                        Requests Status
-                    </div>
-                    <Table columns={columns} dataSource={data} size="small" />
-                </div>
-            </div>
-        </div>
-    );
+  return reuestsList;
 }
+
 export default Maintenance;

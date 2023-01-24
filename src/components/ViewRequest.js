21957c0e5cac2eeaced61e175ae65a7558a90129
Diana Wu <dianawu005@gmail.com>
2023-01-23 20:37:03 -0800

    impletement edit and view request functions

--- src/components/ViewRequest.js
+++ src/components/ViewRequest.js
@@ -1,31 +1,45 @@
-import { Modal } from "antd";
+import { Form,Modal} from "antd";
+import FormItem from "antd/es/form/FormItem";
 import { useState } from "react";
 
 const ViewRequest = ({info}) => {
     const[isModalOpen, setIsModalOpen] = useState(false);
-    
+    const defaultData = info;
     const showModal = () => {
         setIsModalOpen(true);
     };
     const handleModalCancel = () => {
         setIsModalOpen(false);
     };
-    const renderContent = () => {
-        const id = info.requestId;
-        return (
-            <>
-            <div>{id}</div>
-            <div>{info.title}</div>
-            </>
-        );
-    }
+    
     return(
         <>
         <a type="link" onClick={showModal}>
             View
         </a>
         <Modal title="View Request Details" open={isModalOpen} onCancel={handleModalCancel} footer={null}>
-            {renderContent()}
+            <Form labelCol={{span: 6}} wrapperCol={{span: 18}} 
+                style={{maxWidth: 600}} size="middle"
+            >
+                <FormItem name="requestId" label="Request#">
+                    <text style={{padding: "0px"}}>{defaultData.requestId}</text>
+                </FormItem>
+                <FormItem name="date" label="Submit Date">
+                    <text style={{padding: "0px"}}>{defaultData.date}</text>
+                </FormItem>
+                <FormItem name="status" label="Status">
+                    <text style={{padding: "0px"}}>{defaultData.status}</text>
+                </FormItem>
+                <Form.Item name="title" label="Title" >
+                    <text style={{padding: "0px"}}>{defaultData.title}</text>
+                </Form.Item>
+                <Form.Item name="category" label="Category" >
+                    <text style={{padding: "0px"}}>{defaultData.category}</text>
+                </Form.Item>
+                <Form.Item name="description" label="Description" >
+                    <text style={{padding: "0px", height: 150}}>{defaultData.description}</text>
+                </Form.Item>
+            </Form>
         </Modal>
         </>
     );

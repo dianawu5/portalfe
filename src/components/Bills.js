7514cf83d51573fa40a0d1aeba1a00de8502f91a
Jane Zhou <jzhou.jane@gmail.com>
2023-01-29 23:57:05 -0800

    add outstanding balance

--- src/components/Bills.js
+++ src/components/Bills.js
@@ -2,26 +2,41 @@ import React from 'react';
 import requestsList from '../mock-bills.json';
 import { Button, message, Space, Table, Popconfirm, Modal,} from 'antd';
 import { useEffect, useState } from 'react';
-import ViewRequest from './ViewRequest';
 import PayBill from './PayBill';
 import ViewBill from './ViewBill';
 
 const Bills = () => {
 
-    const[data, setData]=useState(requestsList);
+    const[data, setData] = useState(requestsList);
+    const unpaidTotalAmount = data.filter(data => data.status == "unpaid").map( data => data.invoiceAmount ).reduce((a,b)=> a + b, 0);
 
       // to define each column
     const columns = [
     {
-      title: 'Submit Date',
-      dataIndex: 'date',
-      key: 'date',
+      title: 'Invoice#',
+      dataIndex: 'invoiceId',
+      key: 'invoiceId',
       width: 120,
     },
     {
-      title: 'Title',
-      dataIndex: 'title',
-      key: 'title',
+      title: 'Invoice Date',
+      dataIndex: 'invoiceDate',
+      key: 'invoiceDate',
+    },
+    {
+        title: 'Due Date',
+        dataIndex: 'dueDate',
+        key: 'dueDate',
+    },
+    {
+        title: 'Invoice',
+        dataIndex: 'description',
+        key: 'description',
+    },
+    {
+        title: 'Amount',
+        dataIndex: 'invoiceAmount',
+        key: 'invoiceAmount',
     },
     {
       title: 'Status',
@@ -30,16 +45,12 @@ const Bills = () => {
       width: 120,
       filters:[
         {
-          text: "Paid",
-          value:"Paid",
+          text: "paid",
+          value:"paid",
         },
         {
-          text:"Unpaid",
-            value:"Unpaid",
-        },
-        {
-          text:"In Process",
-            value:"In Process",
+          text:"unpaid",
+            value:"unpaid",
         },
       ],
       onFilter: (value, record) => record.status.indexOf(value) === 0,
@@ -50,26 +61,29 @@ const Bills = () => {
       width: 250,
 
       render: (_, record) => {
-        if(record.status === "Unpaid") {
+        if(record.status === "unpaid") {
             return <PayBill info={record} />
-        } else if(record.status === "Paid") {
-            return <ViewBill info={record} />
-        }
+        } 
       }
     },
   ];
 
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
     return(
         <div>
-        <div style={{ fontSize: 20, fontWeight: 600,paddingBottom:"20px" }}>
-            Bills
+            <div style={{ fontSize: 20, fontWeight: 600,paddingBottom:"20px" }}>
+            View Bills
+            <div style={{padding: "5px"}} align="end">
+                    <p>Outstanding Balance</p>
+                    <p>{formatter.format(unpaidTotalAmount)}</p>
+                </div>
             </div>
             <div style={{ fontSize: 20, fontWeight: 600,paddingLeft:"15%" }}>
-                <div style={{ padding:"5px",backgroundColor:"rgba(55, 88, 70,.1)"}}>
-                    <div style={{ fontSize: 18, fontWeight: 600,padding:"5px",backgroundColor:"rgba(55, 88, 70,.1)"}}
-                    >
-                        Payment
-                    </div>
+                <div style={{ padding:"20px",backgroundColor:"rgba(55, 88, 70,.1)"}}>
                     <Table columns={columns} dataSource={data} size="small" />
                 </div>
             </div>

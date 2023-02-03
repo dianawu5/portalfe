4192fa7df0e4f1347be60172adb5145a21a77348
Diana Wu <dianawu005@gmail.com>
2023-02-02 21:36:51 -0800

    Add contact page

---
+++ src/components/Contacts.js
@@ -0,0 +1,43 @@
+
+const Contacts = () => (
+    <div>
+        <div style={{ fontSize: 20, fontWeight: 600,paddingBottom:"30px" }}>
+        Contact Us
+        </div>
+        <div style={{ fontSize: 18, fontWeight: 600,paddingLeft:"15%" }}>
+
+
+            <div style={{paddingTop:"20px", paddingLeft:"0px",padding: "10px", fontSize: 20}}>Happy Community at LaiOffer Place</div>
+            
+            <div style={{padding: "20px"}}>
+                <div style={{fontStyle: "italic"}}>Location</div>
+                <div style={{fontSize: 16, fontWeight: 500, paddingLeft:"5px"}}>
+                    <div>2350 Mission College Blvd</div>
+                    <div>Santa Clara, CA 95054</div>
+                </div>
+            </div>
+            <div style={{padding: "20px"}}>
+                <div style={{fontStyle: "italic"}}> Office</div>
+                <div style={{fontSize: 16, fontWeight: 500, paddingLeft:"5px"}}> 1st Floor of Building#1</div>
+            </div>
+            <div style={{padding: "20px"}}>
+                <div style={{fontStyle: "italic"}}>Hours</div>
+                <div style={{fontSize: 16, fontWeight: 500, paddingLeft:"5px"}}>
+                    <div>Monday - Friday: 9 AM - 10 PM</div>
+                    <div>Saturday - Sunday:	9 AM - 6 PM</div>
+                </div>
+            </div>    
+            <div style={{padding: "20px"}}>
+                <div style={{fontStyle: "italic"}}>Contacts</div>
+                <div style={{fontSize: 16, fontWeight: 500, paddingLeft:"5px"}}>
+                    <div>Office (800) 001-0001</div>
+                    <div>Fax (800) 001-0003</div>
+                    <div>Email info@happycommunity.com</div>
+                </div>
+            </div> 
+        </div>  
+
+    </div>
+);
+
+export default Contacts;
